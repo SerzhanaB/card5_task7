e5209273b738c8661edb19de10689e2d915d456f
Serzhana <buraevase@gmail.com>
2021-09-10 21:21:30 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { class: "off", label: "Отображаемый текст на кнопке" };
+    this.press = this.press.bind(this);
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log("component Did mount");
+  }
+
+  shouldComponentUpdate() {
+    console.log("should Component Update");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("component Did update");
+  }
+
+  componentWillUnmount() {
+    console.log("component Will Unmount");
+  }
+
+press() {
+
+  var className =(this.state.class==="off") ? "on": "off"; 
+  this.setState({class:className});
+}
+
+render () {
+
+  console.log("render");
+  return <button onClick={this.press} className={this.state.class}> {this.state.label} </button>;
+
+}
 }
 
 export default App;
